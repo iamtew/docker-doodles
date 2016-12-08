346c6b8a3802232c6f2a7cefb03062941dc02e16
Mikael Sörlin <mikael@magnetic.io>
2016-12-08 15:05:35 +0100

    Update

--- build.sh
+++ build.sh
@@ -9,23 +9,32 @@ IMAGES="$( find "$DIR" -type f -name Dockerfile | sed 's/Dockerfile$//' )"
 pushd() { command pushd "$@" &> /dev/null; }
 popd()  { command popd "$@" &> /dev/null; }
 
-build_and_push() {
+docker_build() {
   [[ -d $1 ]] || return 1
   local image="$( basename $1 )"
   pushd $image
 
-  echo "Building and pushing '$image' as '${ORG}/${REPO}:$image' ..."
+  echo "Building '$image' as '${ORG}/${REPO}:$image' ..."
 
   docker build --tag "${ORG}/${REPO}:$image" .
+  popd
+}
+
+docker_push() {
+  [[ -d $1 ]] || return 1
+  local image="$( basename $1 )"
+  pushd $image
+
+  echo "Pushing '$image' as '${ORG}/${REPO}:$image' ..."
+
   docker push "${ORG}/${REPO}:$image"
   popd
 }
 
-
 if [[ $1 ]]; then
-  build_and_push "$1"
+  docker_build "$1"
 else
   for image in $IMAGES ; do
-    build_and_push "$image"
+    docker_build "$image"
   done
 fi
